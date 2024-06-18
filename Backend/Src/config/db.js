a6f90f31da8cadddf37eaa8656dd28cd43308ee2
Nilushiya <nilushiyak@gmail.com>
2024-06-18 10:34:17 +0530

    first commit

---
+++ Backend/Src/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const connection = async() => {
+    try{
+            await mongoose.connect('mongodb://localhost:27017/Image_Pdf_Store',{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+    });
+    console.log('MongoDB connected');
+    }
+    catch(err){
+        console.log('MongoDB connection error:', err);
+    }
+}
